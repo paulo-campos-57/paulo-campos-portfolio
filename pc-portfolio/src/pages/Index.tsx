2a955b1586acd7fb27f41b490963b420fa6264a0
paulo-campos-57 <paulo.m.campos6601@gmail.com>
2026-03-17 09:20:15 -0300

    add: begin index page

---
+++ pc-portfolio/src/pages/Index.tsx
@@ -0,0 +1,9 @@
+export default function Index() {
+    return (
+        <>
+            <div className="bg-black w-full h-full flex items-center justify-center">
+                <h1 className="text-white text-4xl">Welcome to my Portfolio</h1>
+            </div>
+        </>
+    );
+}
